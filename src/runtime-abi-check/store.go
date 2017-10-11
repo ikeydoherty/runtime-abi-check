2de14b501d456f1a5982ed8565d190182f020a05
Ikey Doherty <ikey@solus-project.com>
2017-10-11 23:59:17 +0100

    Add super derpy path resolver to find potential library location
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/runtime-abi-check/store.go
+++ src/runtime-abi-check/store.go
@@ -19,6 +19,8 @@ package main
 import (
 	"debug/elf"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 // SymbolStore is used to create a global mapping so that we can resolve symbols
@@ -27,12 +29,49 @@ type SymbolStore struct {
 	// symbols map Machine -> library name -> symbol
 	// TODO: Consider making this full library path to symbol and resolve that way..
 	symbols map[elf.Machine]map[string]string
+
+	// Where we're allowed to look for system libraries.
+	systemLibraries []string
 }
 
 // NewSymbolStore will return a newly setup symbol store..
 func NewSymbolStore() *SymbolStore {
 	ret := &SymbolStore{
 		symbols: make(map[elf.Machine]map[string]string),
+		// Typical set of paths known by linux distributions
+		systemLibraries: []string{
+			"/usr/lib64",
+			"/usr/lib",
+			"/usr/lib/x86_64-linux-gnu",
+			"/usr/lib/i386-linux-gnu",
+			"/usr/lib32",
+		},
+	}
+
+	return ret
+}
+
+// locateLibrary is a private method to determine where a library might actually
+// be found on the system
+func (s *SymbolStore) locateLibrary(library string, inputFile *elf.File) []string {
+	var ret []string
+	var searchPath []string
+	// TODO: Be unstupid and accept DT_RUNPATH foo as well as faked LD_LIBRARY_PATH
+	searchPath = append(searchPath, s.systemLibraries...)
+	for _, p := range searchPath {
+		// Find out if the guy exists.
+		fullPath := filepath.Join(p, library)
+		st, err := os.Stat(fullPath)
+		if err != nil {
+			continue
+		}
+
+		// Using stat not lstat..
+		if !st.Mode().IsRegular() {
+			continue
+		}
+		ret = append(ret, fullPath)
+
 	}
 	return ret
 }
@@ -52,7 +91,10 @@ func (s *SymbolStore) ScanPath(path string) error {
 	}
 
 	// At this point, we'd load all relevant libs
-	fmt.Println(libs)
+	for _, l := range libs {
+		possibles := s.locateLibrary(l, file)
+		fmt.Println(possibles)
+	}
 
 	// Figure out what symbols we end up using
 	syms, err := file.ImportedSymbols()
